be728239b0398279ee0096659a7f109fd0a9a074
AbdulRafay08 <rafay51698@gmail.com>
2021-12-04 14:04:38 +0500

    Incomplete 2nd commit

--- lib/model/model.dart
+++ lib/model/model.dart
@@ -1,41 +1,41 @@
-// import 'package:flutter/material.dart';
+// // import 'package:flutter/material.dart';
 
-class WeatherModel {
-  // final DateTime description;
-  String? city;
-  // final double temp;
-  String? humidity;
+// class WeatherModel {
+//   // final DateTime description;
+//   String? city;
+//   // final double temp;
+//   String? humidity;
 
-  WeatherModel({
-    // required this.description,
-    this.city,
-    // required this.temp,
-    this.humidity,
-  });
-}
-// class Weather {
-//   final DateTime date;
-//   final String name;
-//   final double temp;
-//   final String main;
-//   final String icon;
-
-//   Weather(
-//       {required this.date,
-//       required this.name,
-//       required this.temp,
-//       required this.main,
-//       required this.icon});
-
-//   factory Weather.fromJson(Map<String, dynamic> json) {
-//     return Weather(
-//       date:
-//           DateTime.fromMillisecondsSinceEpoch(json['dt'] * 1000, isUtc: false),
-//       name: json['name'],
-//       temp: double.parse(
-//           ((json['main']['temp'].toDouble()) - 273.15).toStringAsFixed(2)),
-//       main: json['weather'][0]['main'],
-//       icon: json['weather'][0]['icon'],
-//     );
-//   }
+//   WeatherModel({
+//     // required this.description,
+//     this.city,
+//     // required this.temp,
+//     this.humidity,
+//   });
 // }
+// // class Weather {
+// //   final DateTime date;
+// //   final String name;
+// //   final double temp;
+// //   final String main;
+// //   final String icon;
+
+// //   Weather(
+// //       {required this.date,
+// //       required this.name,
+// //       required this.temp,
+// //       required this.main,
+// //       required this.icon});
+
+// //   factory Weather.fromJson(Map<String, dynamic> json) {
+// //     return Weather(
+// //       date:
+// //           DateTime.fromMillisecondsSinceEpoch(json['dt'] * 1000, isUtc: false),
+// //       name: json['name'],
+// //       temp: double.parse(
+// //           ((json['main']['temp'].toDouble()) - 273.15).toStringAsFixed(2)),
+// //       main: json['weather'][0]['main'],
+// //       icon: json['weather'][0]['icon'],
+// //     );
+// //   }
+// // }
